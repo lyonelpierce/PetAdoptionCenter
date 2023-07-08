08c5458f58848167bb6686edfbb2a4c6785e34ac
Lyonel Pierce <lyonel@live.com>
2023-07-08 12:25:21 -0400

    Railway Deployment Url

--- backend/src/main/java/com/lyonelpierce/animaladoption/configurations/WebConfig.java
+++ backend/src/main/java/com/lyonelpierce/animaladoption/configurations/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://pet-adoption-center-rho.vercel.app/");
+        config.addAllowedOrigin("https://pawsome.up.railway.app/");
         config.addAllowedOrigin("http://localhost:3000");
         config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT));
         config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name()));
