4627078fcb145cbdfa00a50603d17df0c29bec81
Lyonel Pierce <lyonel@live.com>
2023-07-08 10:58:55 -0400

    Modal Close Button

--- frontend/components/PetList.jsx
+++ frontend/components/PetList.jsx
@@ -137,6 +137,25 @@ const PetList = () => {
         >
           {selectedPet && (
             <>
+              <button
+                className="sm:hidden absolute top-2 right-2 bg-gray-300 rounded-full p-2"
+                onClick={handleClose}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
               <h2 className="text-center text-2xl font-medium mb-7">
                 Request Adoption
               </h2>
